9dd63aed7b81476d4ef25307ff6993d03272e7e6
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-03-04 13:23:38 +0100

    Created project 0102_DevToolsDemo

---
+++ 01_SpringBootOverview/02_DevToolsDemo/src/test/java/com/drobledo/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drobledo.springboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
