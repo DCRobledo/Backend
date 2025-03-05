7406680e7202089b28453068793dbbc758c7b74f
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-03-05 12:05:42 +0100

    Created project 03_SpringBootHibernateJDACRUD

---
+++ 03_SpringBootHibernateJPACRUD/src/test/java/com/drobledo/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drobledo.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
